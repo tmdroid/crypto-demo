acd6a98743c0521e760d5705408483015d0cc2ab
daniel <daniel@simvelop.de>
2024-04-26 13:34:14 +0300

    fix ios implementation

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,4 +1,9 @@
 import androidx.compose.ui.window.ComposeUIViewController
+import di.initializeKoin
 import presentation.App
+import presentation.tickers.TickersScreenView
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    val tickersScreenView = initializeKoin().koin.get<TickersScreenView>()
+    App(tickersScreenView)
+}
