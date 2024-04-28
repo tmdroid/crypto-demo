78e3eb070fbc5f22a9d8054bd00c1bc7c3aec99f
daniel <daniel@simvelop.de>
2024-04-28 15:33:13 +0300

    align ios app to the new details view

--- composeApp/src/iosMain/kotlin/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/MainViewController.kt
@@ -1,9 +1,13 @@
 import androidx.compose.ui.window.ComposeUIViewController
 import di.initializeKoin
 import presentation.App
+import presentation.tickerdetails.TickerDetailsScreenView
 import presentation.tickers.TickersScreenView
+import util.getScreenView
 
 fun MainViewController() = ComposeUIViewController {
-    val tickersScreenView = initializeKoin().koin.get<TickersScreenView>()
-    App(tickersScreenView)
+    val koin = initializeKoin()
+    val tickersScreenView = koin.getScreenView<TickersScreenView>()
+    val tickerDetailsScreenView = koin.getScreenView<TickerDetailsScreenView>()
+    App(tickersScreenView, tickerDetailsScreenView)
 }
